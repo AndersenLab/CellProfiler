8ddfe0fd33ac171c6e1c3180efd56d495fbb6236
Sam Widmayer <sam.widmayer@northwestern.edu>
2019-12-11 14:55:37 -0600

    fancy header

--- scripts/run.cell.profiler.sh
+++ scripts/run.cell.profiler.sh
@@ -4,9 +4,10 @@
 # bash scripts/run.cell.profiler.sh projects/20190926_drugresponse/
 
 export PROJECT_ID=$1
-export CPBIN=/projects/b1059/software/CellProfiler
 export IMAGES=${PROJECT_ID}/raw_images/
-
+export PROJECT_TITLE=$(echo ${PROJECT_ID} | cut -f2 -d "/")
+export OUTPUT_DATA=${PROJECT_ID}/cellprofiler_output_data
+export CPBIN=/projects/b1059/software/CellProfiler
 
 echo "
 ####  ####  #     #     ####  ####   ####  ####  ##  #     ####  ####
@@ -17,13 +18,10 @@ echo "
 "
 echo "Home Directory: ${HOME}"
 echo "Path to CellProfiler Software: ${CPBIN}"
-
-NIMAGES=$(ls ${IMAGES} | wc -l)
 echo "LOG:     Begin CellProfiler Analysis"
-echo "LOG:     Number of Images: ${NIMAGES}"
-
-export PROJECT_TITLE=$(echo ${PROJECT_ID} | cut -f2 -d "/")
 echo "LOG:     Project Title: ${PROJECT_TITLE}"
+NIMAGES=$(ls ${IMAGES} | wc -l)
+echo "LOG:     Number of Images: ${NIMAGES}"
 
 if [ ! -d ${PROJECT_ID}/cellprofiler_output_data ]; then
     echo "LOG:     CellProfiler Output Directory Not Found :/" ;
@@ -32,14 +30,13 @@ if [ ! -d ${PROJECT_ID}/cellprofiler_output_data ]; then
     echo "LOG:     CellProfiler Analysis Output Directory Exists - Nice!" ;
 fi
 
-export OUTPUT_DATA=${PROJECT_ID}/cellprofiler_output_data
 mkdir ${OUTPUT_DATA}/${PROJECT_TITLE}_summary_data
 
-# COUNT=1; \
-# for i in $(seq $NIMAGES); \
-#  do sbatch ${CPBIN}/scripts/cellprofiler.20191204.sh $i; \
-#  let COUNT=$COUNT+1; \
-#done
+COUNT=1; \
+for i in $(seq $NIMAGES); \
+do sbatch ${CPBIN}/scripts/cellprofiler.20191204.sh $i; \
+let COUNT=$COUNT+1; \
+done
 
 # NEXT STEP IN PIPELINE
 # random file to pull table header from
