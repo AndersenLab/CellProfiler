68cddfad4466afea41d61730121cc8b735d8052c
Sam Widmayer <sam.widmayer@northwestern.edu>
2019-12-20 09:24:16 -0600

    typo

--- scripts/concatenate.triplepipeline.results.sh
+++ scripts/concatenate.triplepipeline.results.sh
@@ -10,7 +10,6 @@ export SUMMARY_DATA=${OUTPUT_DATA}/${PROJECT_TITLE}_summary_data
 
 # CONTROL MODEL OUTPUTS
 cd ${SUMMARY_DATA}/NonOverlappingWorms_Data
-ls *control.csv > control.list.txt
 files=(*_control.csv)
 less ${files[RANDOM % ${#files[@]}]} | head -1 > ${PROJECT_TITLE}_all_control1.csv
 tail -n +2 -q *_control.csv > ${PROJECT_TITLE}_all_control2.csv
@@ -20,7 +19,6 @@ rm ${PROJECT_TITLE}_all_control1.csv
 rm ${PROJECT_TITLE}_all_control2.csv
 
 # FULL MODEL OUTPUTS
-ls *full.csv > full.list.txt
 files=(*_full.csv)
 less ${files[RANDOM % ${#files[@]}]} | head -1 > ${PROJECT_TITLE}_all_full1.csv
 tail -n +2 -q *_full.csv > ${PROJECT_TITLE}_all_full2.csv
@@ -30,7 +28,6 @@ rm ${PROJECT_TITLE}_all_full1.csv
 rm ${PROJECT_TITLE}_all_full2.csv
 
 # HIGH MODEL OUTPUTS
-ls *high.csv > high.list.txt
 files=(*_high.csv)
 less ${files[RANDOM % ${#files[@]}]} | head -1 > ${PROJECT_TITLE}_all_high1.csv
 tail -n +2 -q *_high.csv > ${PROJECT_TITLE}_all_high2.csv
@@ -44,7 +41,6 @@ rm ${PROJECT_TITLE}_all_high2.csv
 
 # CONTROL MODEL OUTPUTS
 cd ${SUMMARY_DATA}/OverlappingWorms_Data
-ls *control.csv > control.list.txt
 files=(*_control.csv)
 less ${files[RANDOM % ${#files[@]}]} | head -1 > ${PROJECT_TITLE}_all_control1.csv
 tail -n +2 -q *_control.csv > ${PROJECT_TITLE}_all_control2.csv
@@ -54,7 +50,6 @@ rm ${PROJECT_TITLE}_all_control1.csv
 rm ${PROJECT_TITLE}_all_control2.csv
 
 # FULL MODEL OUTPUTS
-ls *full.csv > full.list.txt
 files=(*_full.csv)
 less ${files[RANDOM % ${#files[@]}]} | head -1 > ${PROJECT_TITLE}_all_full1.csv
 tail -n +2 -q *_full.csv > ${PROJECT_TITLE}_all_full2.csv
@@ -64,7 +59,6 @@ rm ${PROJECT_TITLE}_all_full1.csv
 rm ${PROJECT_TITLE}_all_full2.csv
 
 # HIGH MODEL OUTPUTS
-ls *high.csv > high.list.txt
 files=(*_high.csv)
 less ${files[RANDOM % ${#files[@]}]} | head -1 > ${PROJECT_TITLE}_all_high1.csv
 tail -n +2 -q *_high.csv > ${PROJECT_TITLE}_all_high2.csv
